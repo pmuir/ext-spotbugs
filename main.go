2089be9e56d0a3e340383456c2c7a0ec7ee7b406
Pete Muir <pmuir@bleepbleep.org.uk>
2018-10-12 11:22:06 +0100

    feat: upgrade to fact model

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ import (
 	"github.com/pkg/errors"
 )
 
+// TODO replace these with imports from jx
+const FactTypeStaticProgramAnalysis = "jx.staticProgramAnalysis"
+const MeasurementCount   = "count"
+
 func watch() (err error) {
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
@@ -45,7 +49,6 @@ func watch() (err error) {
 			log.Fatalf("unexpected type %s\n", event)
 		}
 		//
-		if len(act.Spec.Summaries.StaticProgramAnalysis.Tags) == 0 {
 			for _, attachment := range act.Spec.Attachments {
 				if attachment.Name == "spotbugs" {
 					// TODO Handle having multiple attachments properly
@@ -55,37 +58,63 @@ func watch() (err error) {
 						if err != nil {
 							log.Println(errors.Wrap(err, fmt.Sprintf("Unable to retrieve %s for processing", url)))
 						}
-						// Create the summaries for the categories
-						categories := make(map[string]jenkinsv1.StaticProgramAnalysisCategory)
+						found := make([]jenkinsv1.Fact, 0)
+						for _, f := range act.Spec.Facts {
+							if f.FactType == FactTypeStaticProgramAnalysis {
+								found = append(found, f)
+								break
+							}
+						}
+						if len(found) > 1 {
+							return errors.New(fmt.Sprintf("More than one fact of kind %s found %s", FactTypeStaticProgramAnalysis, found))
+						}
+
+						fact := jenkinsv1.Fact{}
+						if fact.Name == "" {
+							fact.FactType = FactTypeStaticProgramAnalysis
+							fact.Original = jenkinsv1.Original{
+								URL:      url,
+								MimeType: "application/xml",
+								Tags: []string{
+									"spotbugsXml.xml",
+								},
+							}
+							fact.Tags = []string{
+								"spotbugs",
+							}
+							act.Spec.Facts = append(act.Spec.Facts, fact)
+						}
+						categories := make(map[string]map[string]int, 0)
+						measurements := make([]jenkinsv1.Measurement, 0)
 						for _, b := range bugCollection.BugInstance {
 							category, ok := categories[b.Category]
 							if !ok {
-								category = jenkinsv1.StaticProgramAnalysisCategory{}
+								category = make(map[string]int, 0)
 							}
 							switch b.Priority {
 							case 1:
-								category.HighPriority++
+								category[jenkinsv1.StaticProgramAnalysisHighPriority]++
 							case 2:
-								category.NormalPriority++
+								category[jenkinsv1.StaticProgramAnalysisNormalPriority]++
 							case 3:
-								category.LowPriority++
+								category[jenkinsv1.StaticProgramAnalysisLowPriority]++
 							case 5:
-								category.Ignored++
+								category[jenkinsv1.StaticProgramAnalysisIgnored]++
 							}
 							categories[b.Category] = category
 						}
-						act.Spec.Summaries.StaticProgramAnalysis = jenkinsv1.StaticProgramAnalysis{
-							Tags:           []string{
-								attachment.Name,
-							},
-							TotalBugs:      bugCollection.FindBugsSummary.TotalBugs,
-							HighPriority:   bugCollection.FindBugsSummary.HighPriority,
-							NormalPriority: bugCollection.FindBugsSummary.NormalPriority,
-							LowPriority:    bugCollection.FindBugsSummary.LowPriority,
-							Ignored:        bugCollection.FindBugsSummary.IgnorePriority,
-							TotalClasses:   bugCollection.FindBugsSummary.TotalClasses,
-							Categories:     categories,
+						for k, v := range categories {
+							for l, w := range v {
+								measurements = append(measurements, createMeasurement(k, l, w))
+							}
 						}
+						measurements = append(measurements, createMeasurement("summary", jenkinsv1.StaticProgramAnalysisTotalBugs, bugCollection.FindBugsSummary.TotalBugs))
+						measurements = append(measurements, createMeasurement("summary", jenkinsv1.StaticProgramAnalysisHighPriority, bugCollection.FindBugsSummary.HighPriority))
+						measurements = append(measurements, createMeasurement("summary", jenkinsv1.StaticProgramAnalysisNormalPriority, bugCollection.FindBugsSummary.NormalPriority))
+						measurements = append(measurements, createMeasurement("summary", jenkinsv1.StaticProgramAnalysisLowPriority, bugCollection.FindBugsSummary.LowPriority))
+						measurements = append(measurements, createMeasurement("summary", jenkinsv1.StaticProgramAnalysisIgnored, bugCollection.FindBugsSummary.IgnorePriority))
+						measurements = append(measurements, createMeasurement("summary", jenkinsv1.StaticProgramAnalysisTotalClasses, bugCollection.FindBugsSummary.TotalClasses))
+						fact.Measurements = measurements
 						act, err = client.PipelineActivities(act.Namespace).Update(act)
 						log.Printf("Updated PipelineActivity %s with data from %s\n", act.Name, url)
 						if err != nil {
@@ -94,7 +123,7 @@ func watch() (err error) {
 					}
 				}
 			}
-		}
+
 	}
 	return nil
 }
@@ -117,6 +146,14 @@ func parseSpotBugsReport(url string, httpClient *http.Client) (collection findbu
 		return findbugs.BugCollection{}, err
 	}
 	return collection, nil
+}
+
+func createMeasurement(t string, measurement string, value int) jenkinsv1.Measurement {
+	return jenkinsv1.Measurement{
+		Name:             fmt.Sprintf("%s-%s", t, measurement),
+		MeasurementType:  MeasurementCount,
+		MeasurementValue: value,
+	}
 }
 
 func main() {
