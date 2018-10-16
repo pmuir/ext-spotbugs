9388dee3361187b9f41399f4edbcb979ac6d2000
Pete Muir <pmuir@bleepbleep.org.uk>
2018-10-16 01:00:49 +0100

    fix: use env var for name

--- spotbugs_analyzer_upgrade.sh
+++ spotbugs_analyzer_upgrade.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+NAME=ext-spotbugs
 helm repo update
-helm install ${JX_SPOTBUGS_ANALYZER_UPGRADE_CHART_REPOSITORY}/ext-spotbugs --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE} --name=ext-jacoco
+helm install ${JX_SPOTBUGS_ANALYZER_UPGRADE_CHART_REPOSITORY}/${NAME} --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE} --name=${NAME}
