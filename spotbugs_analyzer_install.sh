a5f8c3319f7466914758363521da91e1f2e13758
Pete Muir <pmuir@bleepbleep.org.uk>
2018-11-06 18:24:41 +0000

    fix: let’s use URLs for repos, not short names

--- spotbugs_analyzer_install.sh
+++ spotbugs_analyzer_install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 NAME=ext-spotbugs
-helm install ${JX_SPOTBUGS_ANALYZER_INSTALL_CHART_REPOSITORY}/${NAME} --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE} --name=${NAME}
+helm install --repo ${JX_SPOTBUGS_ANALYZER_INSTALL_CHART_REPOSITORY} ${NAME} --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE} --name=${NAME}
--- spotbugs_analyzer_upgrade.sh
+++ spotbugs_analyzer_upgrade.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 NAME=ext-spotbugs
-helm repo update
-helm install ${JX_SPOTBUGS_ANALYZER_UPGRADE_CHART_REPOSITORY}/${NAME} --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE} --name=${NAME}
+helm upgrade ${NAME} --repo ${JX_SPOTBUGS_ANALYZER_UPGRADE_CHART_REPOSITORY} ${NAME} --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE}
